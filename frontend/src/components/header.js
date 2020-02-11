1d4934469c8ceb521bc992542e51a8ee916fca5b
mmhasnat17 <hasnat250@gmail.com>
2020-02-11 16:41:00 -0500

    landing page

---
+++ frontend/src/components/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Community</h1>
+        <p>Impacting Backbay & SouthEnd resident lives</p>
+        <div>
+          <button type="button">Sign Up Here!</button>
+        </div>
+        <div>
+          <button type="button">Login</button>
+        </div>
+        <img src="./assets/backbaypark.png" />
+      </header>
+    );
+  }
+}
+
+export default Header;
