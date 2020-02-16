881dcd2bd3938145042ea6803832b45ebb29e8be
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-02-16 14:59:18 -0500

    Fix format/deleting Gender category

--- frontend/src/Signuppage/Loginform.js
+++ frontend/src/Signuppage/Loginform.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import "./Loginform.css"
+
 
 import { Button, Form, Formgroup, Label, Input } from "react-bootstrap";
 import { render } from "react-dom";
@@ -16,14 +18,14 @@ export class Login extends React.Component {
         <div className="content">
           <form className="contentform">
             {/* <div class="form-group-gender">
-              <label for="exampleFormControlSelect1">Gender:</label>
+              <label htmlFor="exampleFormControlSelect1">Gender:</label>
               <select class="form-control" id="exampleFormControlSelect1">
                 <option selected="" value="Default"></option>
                 <option>Male</option>
                 <option>Female</option>
                 <option>Rather not say</option>
               </select>
-            </div> */}
+            </div> }
 
             {/* <div class="form-group-username">
               <div class="input-group-prepend">
@@ -37,55 +39,53 @@ export class Login extends React.Component {
               ></input>
             </div> */}
 
-            <div class="form-group-name">
-              <div class="input-group-prepend">
+            <div className="form-group-name">
+              <div className="input-group-prepend">
                 <span>Person</span>
               </div>
 
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="First Name"
               ></input>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="Last Name"
               ></input>
             </div>
-            <div class="form-group-email">
-              <label for="exampleFormControlInput1">Email address</label>
+            <div className="form-group-email">
+              <label htmlFor="exampleFormControlInput1">Email address</label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleFormControlInput1"
                 placeholder="name@example.com"
               ></input>
             </div>
 
-            <div class="form-group-password">
-              <label for="exampleInputPassword1">Password</label>
+            <div className="form-group-password">
+              <label htmlFor="exampleInputPassword1">Password</label>
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="exampleInputPassword1"
                 placeholder="Password"
               ></input>
             </div>
 
-            <div class="form-group-question">
-              <label for="exampleFormControlSelect1">Do you have a dog? </label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option selected="" value="Default"></option>
+            <div className="form-group-question">
+              <label htmlFor="exampleFormControlSelect1">Do you have a dog? </label>
+              <select defaultValue="Default"className="form-control" id="exampleFormControlSelect1">
                 <option>Yes</option>
                 <option>No</option>
               </select>
             </div>
 
-            <div class="form-group-dogbreed">
-              <label for="exampleFormControlSelect1">Your's dog breed: </label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option selected="" value="Default"></option>
+            <div className="form-group-dogbreed">
+              <label htmlFor="exampleFormControlSelect1">Your's dog breed: </label>
+              <select defaultValue="Default" className="form-control" id="exampleFormControlSelect1">
                 <option value="gt">Golden Retriver</option>
                 <option value="bt">Boston Terrier</option>
                 <option value="bd">Bull Dog</option>
@@ -98,18 +98,18 @@ export class Login extends React.Component {
               </select>
             </div>
 
-            <div class="form-group-extrainformation">
-              <label for="exampleFormControlTextarea1">
+            <div className="form-group-extrainformation">
+              <label htmlFor="exampleFormControlTextarea1">
                 Tell us more about your dog:
               </label>
               <textarea
-                class="form-control"
+                className="form-control"
                 id="exampleFormControlTextarea1"
                 rows="3"
               ></textarea>
             </div>
 
-            <Button variant="primary" type="submit" class="btn btn-primary">
+            <Button variant="primary" type="submit" className="btn btn-primary">
               Submit
             </Button>
           </form>
