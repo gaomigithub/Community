b1117e07543400d9e2d05262723410995fe4190f
Mi Gao <gaomius@hotmail.com>
2020-02-21 17:21:29 -0500

    Fixing path structure

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { HashRouter, Route, Switch, BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Signup from "./signup-page/Loginform";
-import Report from "./signup-page/Signup-report";
+import Signup from "./Loginform";
+import Report from "./Signup-report";
 
 const BasicRoute = () => (
   <HashRouter history={BrowserRouter}>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/signup-page/Loginform" component={Signup} />
-      <Route exact path="/signup-page/Signup-report" component={Report} />
+      <Route exact path="/Loginform" component={Signup} />
+      <Route exact path="/Signup-report" component={Report} />
     </Switch>
   </HashRouter>
 );
