3faf8a3d3fb6b825dbc6a2c4ae4a9997147072a2
Mi Gao <gaomius@hotmail.com>
2020-02-22 16:31:49 -0500

    Fix conflicts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from "react";
-import "./index.css";
-import Main from "./components/main";
-import Header from "./components/header";
-import Nav from "./components/nav";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react"; // or 'aws-amplify-react-native';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Nav />
-        <Header />
-        <Main />
-      </div>
-    );
-  }
+Amplify.configure(awsconfig);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App, true, true);
