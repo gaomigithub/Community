a17692316b468447541b5529a67bcd912368f0d3
Mi Gao <gaomius@hotmail.com>
2020-02-22 16:27:00 -0500

    Delete the old src folder

---
+++ src/Signup-report.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Report extends React.Component {
+  render() {
+    return (
+      <div>
+        <a>
+          Thanks for Join Us! <br />
+          Welcome!
+        </a>
+      </div>
+    );
+  }
+}
