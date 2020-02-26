a148411bb6397c463f653cc6c948c266a6fc0257
zhuochaozhong <zhuochaozhong2017@gmail.com>
2020-02-26 13:48:22 -0500

    Fix create user bug

--- amplify/backend/function/userfunction/src/createUser.js
+++ amplify/backend/function/userfunction/src/createUser.js
@@ -14,19 +14,19 @@ AWS.config.update({region: region});
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 var ddb_table_name = process.env.STORAGE_USERTABLE_NAME
 
-async function createUser(event, callback) { 
+function createUser(event, callback) { 
 
-    const args = { ...event.arguments}
+    const user = event.arguments
   
     var params = {
         TableName: ddb_table_name,
-        Item: args
+        Item: user
     };
     console.log(params);
 
     ddb.putItem(params, function(err, data) {
         if (err) console.log(err, err.stack);
-        else console.log(data); 
+        else console.log("Success Create New User"); 
     });
 }; 
 
