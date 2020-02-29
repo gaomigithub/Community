b1a45d33cf7303dae179c542faefa417d253f2d0
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-02-29 18:45:31 -0500

    Test out Doginfodetails component, add UI to Doginfodetails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
-import { Login } from "./Signuppage/index.js";
+//import { Login } from "./Signuppage/index.js";
+import {DogForm} from './Dogprofile/DogForm';
 
-import { Button, Form, Formgroup, Label, Input } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="APP">
-      <Login />
+import { render } from "@testing-library/react";
+
+//function App() {
+  class App extends Component{ 
+  render() {
+    return (
+    //<div className="APP">
+    // <Login />
+    //</div>
+    <div className="App">
+      <DogForm />
     </div>
-  );
+    );
+  }
 }
 export default App;
---
+++ frontend/src/Dogprofile/dogInfoDetails.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Doginfodetails extends Component{
+    render (){
+        return (
+            <div>
+                <h1>Welcome Puppies</h1>
+            </div>
+        );
+    }
+}
+
+export default Doginfodetails;
