9247afe279c0317d2d3b9d362ce68fa2a52ebde9
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-03-01 05:34:40 -0500

    Add render() and verify if picture is allow to add

--- frontend/src/Dogprofile/DogPictureUpload.js
+++ frontend/src/Dogprofile/DogPictureUpload.js
@@ -1,2 +1,18 @@
-import React, {Component} from 'react';
-import Amplify from 'aws'
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
+
+const imageMaxSize = 1000000000
+const acceptedFileTypes ='image/x-png, image/png, image/jpg,image/jpeg, image/gif'
+const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item)=> {return item.trim()})
+
+    verifyFile = (files) => {
+        if (files && files.length > 0){
+            const currentFile = files[0]
+            const currentFileType = currentFile.type 
+            const currentFileSize = currentFile.size 
+            if(currentFileSize > imageMaxSize){
+                alert("This file is not allowed." + currentFileSize + "bytes is too large")
+                return false
+            }
+        }
+    }
