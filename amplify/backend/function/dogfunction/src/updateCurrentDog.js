e66db3f49c8c65def5a1d95026501f6f92e7d2c8
zhuochaozhong <zhuochaozhong2017@gmail.com>
2020-03-01 12:39:21 -0500

    Add dog table and update function templets

--- amplify/backend/function/dogfunction/src/updateCurrentDog.js
+++ amplify/backend/function/dogfunction/src/updateCurrentDog.js
@@ -2,9 +2,7 @@
 You can access the following resource attributes as environment variables from your Lambda function
 var environment = process.env.ENV
 var region = process.env.REGION
-var apiCommunityGraphQLAPIIdOutput = process.env.API_COMMUNITY_GRAPHQLAPIIDOUTPUT
-var apiCommunityGraphQLAPIEndpointOutput = process.env.API_COMMUNITY_GRAPHQLAPIENDPOINTOUTPUT
-var storageUsertableName = process.env.STORAGE_USERTABLE_NAME
-var storageUsertableArn = process.env.STORAGE_USERTABLE_ARN
+var storageDogtableName = process.env.STORAGE_DOGTABLE_NAME
+var storageDogtableArn = process.env.STORAGE_DOGTABLE_ARN
 
 Amplify Params - DO NOT EDIT */
