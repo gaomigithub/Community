3479e76411965e8bf2fec11b96f1e8a079a4bdbb
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-03-01 05:39:37 -0500

    add imgSrc and constructor props

--- frontend/src/Dogprofile/DogPictureUpload.js
+++ frontend/src/Dogprofile/DogPictureUpload.js
@@ -4,7 +4,13 @@ import Dropzone from 'react-dropzone';
 const imageMaxSize = 1000000000
 const acceptedFileTypes ='image/x-png, image/png, image/jpg,image/jpeg, image/gif'
 const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item)=> {return item.trim()})
-
+class ImgDropAndCrop extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            imgSrc: null
+        }
+    }
     verifyFile = (files) => {
         if (files && files.length > 0){
             const currentFile = files[0]
@@ -14,5 +20,35 @@ const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item)=> {return
                 alert("This file is not allowed." + currentFileSize + "bytes is too large")
                 return false
             }
+            if (!acceptedFileTypesArray.includes(currentFileType)){
+                alert("This file is not allowed. Only images are allowed")
+                return false
+            }
+            return true
         }
     }
+
+    handleOnDrop = (files,rejectedFiles) => {
+        if (rejectedFiles && rejectedFiles.length > 0){
+            this.verifyFile(rejectedFiles)
+        }
+        if (files && files.length > 0){
+            const isVerified = this.verifyFile(files)
+            if (isVerified){
+            }
+        }
+    }
+
+    render (){
+            const {imgSrc} = this.state
+        return(
+            <div>
+                <h1>Drop and Crop</h1>
+                {imgSrc !== null? <img src={imgSrc} />: ''}
+                <Dropzone onDrop={this.handleOnDrop} accept = {acceptedFileTypes} multiple={false} maxSize={imageMaxSize}>Drop file here/>
+            </div>
+        )
+    }
+}
+
+export default ImgDropAndCrop
