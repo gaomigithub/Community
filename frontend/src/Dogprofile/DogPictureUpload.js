327b6e5961c47b0436f478a213d92a75999027cd
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-03-01 06:40:20 -0500

    adding config.js, image works

--- frontend/src/Dogprofile/DogPictureUpload.js
+++ frontend/src/Dogprofile/DogPictureUpload.js
@@ -1,54 +1,67 @@
-import React, { Component } from 'react';
-import Dropzone from 'react-dropzone';
+import React, { Component } from 'react'
+import Spinner from './Spinner'
+import Images from './Images'
+import Buttons from './Buttons'
+import { API_URL } from './config'
 
-const imageMaxSize = 1000000000
-const acceptedFileTypes ='image/x-png, image/png, image/jpg,image/jpeg, image/gif'
-const acceptedFileTypesArray = acceptedFileTypes.split(",").map((item)=> {return item.trim()})
-class ImgDropAndCrop extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            imgSrc: null
-        }
-    }
-    verifyFile = (files) => {
-        if (files && files.length > 0){
-            const currentFile = files[0]
-            const currentFileType = currentFile.type 
-            const currentFileSize = currentFile.size 
-            if(currentFileSize > imageMaxSize){
-                alert("This file is not allowed." + currentFileSize + "bytes is too large")
-                return false
-            }
-            if (!acceptedFileTypesArray.includes(currentFileType)){
-                alert("This file is not allowed. Only images are allowed")
-                return false
-            }
-            return true
-        }
+
+export default class DogPictureUpload extends Component {
+  
+  state = {
+    uploading: false,
+    images: []
+  }
+
+  onChange = e => {
+    const files = Array.from(e.target.files)
+    this.setState({ uploading: true })
+
+    const formData = new FormData()
+
+    files.forEach((file, i) => {
+      formData.append(i, file)
+    })
+
+    fetch(`${API_URL}/image-upload`, {
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(images => {
+      this.setState({ 
+        uploading: false,
+        images
+      })
+    })
+  }
+
+  removeImage = id => {
+    this.setState({
+      images: this.state.images.filter(image => image.public_id !== id)
+    })
+  }
+  
+  render() {
+    const { uploading, images } = this.state
+
+    const content = () => {
+      switch(true) {
+        case uploading:
+          return <Spinner />
+        case images.length > 0:
+          return <Images images={images} removeImage={this.removeImage} />
+        default:
+          return <Buttons onChange={this.onChange} />
+      }
     }
 
-    handleOnDrop = (files,rejectedFiles) => {
-        if (rejectedFiles && rejectedFiles.length > 0){
-            this.verifyFile(rejectedFiles)
-        }
-        if (files && files.length > 0){
-            const isVerified = this.verifyFile(files)
-            if (isVerified){
-            }
-        }
-    }
-
-    render (){
-            const {imgSrc} = this.state
-        return(
-            <div>
-                <h1>Drop and Crop</h1>
-                {imgSrc !== null? <img src={imgSrc} />: ''}
-                <Dropzone onDrop={this.handleOnDrop} accept = {acceptedFileTypes} multiple={false} maxSize={imageMaxSize}>Drop file here/>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <div className='buttons'>
+          {content()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default ImgDropAndCrop
