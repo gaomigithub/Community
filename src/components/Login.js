65910a37d787f94fc912ae6774221d3b4731c0cb
Mi Gao <gaomius@hotmail.com>
2020-02-29 22:33:36 -0500

    Tested Signup page can be modified

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,42 @@
+/* eslint-disable no-unused-expressions */
 import React, { Component } from "react";
 
-import { Authenticator } from "aws-amplify-react";
+import { Authenticator, SignIn } from "aws-amplify-react";
+
+class MyCustomSignUp extends Component {
+  constructor() {
+    super();
+    this.gotoSignIn = this.gotoSignIn.bind(this);
+  }
+
+  gotoSignIn() {
+    // to switch the authState to 'signIn'
+    this.props.onStateChange("signIn", {});
+  }
+
+  render() {
+    return (
+      <div>
+        {/* only render this component when the authState is 'signUp' */}
+        {this.props.authState === "signUp" && (
+          <div>
+            My Custom SignUp Component
+            <button onClick={this.gotoSignIn}>Goto SignIn</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default class Login extends Component {
   render() {
-    return <Authenticator />;
+    return (
+      // The override prop tells the Authenticator that the SignUp component is not hidden but overridden
+      <Authenticator hideDefault={true}>
+        <SignIn />
+        <MyCustomSignUp override={"SignUp"} />
+      </Authenticator>
+    );
   }
 }
