c6ff45544711148805f5114bf9acc741a4455af8
JohnLe9318 <47568065+JohnLe9318@users.noreply.github.com>
2020-03-03 13:19:41 -0500

    re-upload all the dog-profile page after fix git pull origin development problem

---
+++ src/components/DogForm.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import Doginfodetails from './Doginfodetails';
+import Allinfo from './Allinfo'
+class DogForm extends Component {
+    state = {
+        step:1,
+        dogName:'',
+        dogAge:'',
+        dogBreed:'',
+        aboutMe:'',
+    }
+    nextStep = () => {
+        const {step} = this.state;
+        this.setState({
+            step: step +1
+        });
+    }
+    prevStep = () => {
+        const {step} = this.state;
+        this.setState({
+            step:step -1
+        });
+    }
+    handleChange = input  => e => {
+        this.setState({[input]: e.target.value})
+    }
+    showStep = () =>{
+        const {step, dogName, dogAge, dogBreed, aboutMe} = this.state;
+        if (step===1)
+            return(
+                <Doginfodetails 
+                    handleChange = {this.handleChange}
+                    nextStep = {this.nextStep}
+                    dogName = {dogName}
+                    dogAge = {dogAge}
+                    dogBreed = {dogBreed}
+                    aboutMe = {aboutMe}
+                />);
+        if (step===2)
+            return(
+                <Allinfo
+                dogName = {dogName}
+                dogAge = {dogAge}
+                dogBreed = {dogBreed}
+                aboutMe = {aboutMe}/>
+            );
+                    
+    }
+    render(){
+        const {step}= this.state;
+        return(
+             <>
+        <h2>Tell us about your puppies</h2>
+        {this.showStep()}
+             </>
+        );
+    }
+}
+export default DogForm;
---
+++ src/components/Doginfodetails.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+class Doginfodetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    render (){
+        const{handleChange, dogName, dogAge, dogBreed, aboutMe} = this.props;
+        return (
+            <>
+                <h2>Puppy's Information:</h2>
+                <div className="dogInfoContent">
+                <label>
+                    <input
+                    type = "text"
+                    name ="dogName"
+                    placeholder= "Dog Name"
+                    value={dogName}
+                    onChange={handleChange('dogName')}/>
+                </label>
+                <label>
+                    <input
+                    type = "numeric"
+                    name ="dogAge"
+                    placeholder= "Dog Age"
+                    value={dogAge}
+                    onChange={handleChange('dogAge')}>
+                        
+                    </input>
+                </label>
+                <label>
+                    <input
+                    type = "text"
+                    name ="dogBreed"
+                    placeholder= "Breed"
+                    value={dogBreed}
+                    onChange={handleChange('dogBreed')}>
+                        
+                    </input>
+                </label>
+                <label>
+                    <textarea
+                    type = "text"
+                    name ="aboutMe"
+                    placeholder="Tell Us More About Your Pet"
+                    value={aboutMe}
+                    onChange={handleChange('aboutMe')}
+                    rows ="3">
+                        
+                    </textarea>
+                </label>
+                </div>
+                <button className="Next" onClick={this.continue}>
+                    Next >>
+                </button>
+            </>
+        );
+    }
+}
+export default Doginfodetails
