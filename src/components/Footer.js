bf86583d6de36d3c0ce5bd61d813e3f12637ab4c
Mi Gao <gaomius@hotmail.com>
2020-03-03 20:20:43 -0500

    Format conflict

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,64 +1,67 @@
 import React from "react";
 import { render } from "react-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/footer.css";
 
 export default function Footer() {
   return (
-    <footer class="pt-4 my-md-5 pt-md-5 border-top">
-      <div class="row">
-        <div class="col-12 col-md">
-          <img
-            class="mb-2"
-            src="./assets/logo.svg"
-            alt=""
-            width="200"
-            height="60"
-          />
-          <small class="d-block mb-3 text-muted">© 2020-2021</small>
+    <div>
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img
+              class="mb-2"
+              src="./assets/logo.svg"
+              alt=""
+              width="200"
+              height="60"
+            />
+            <small class="d-block mb-3 text-muted">© 2020-2021</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li>
+                <a class="text-muted" href="/">
+                  Reservation
+                </a>
+              </li>{" "}
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>{" "}
+            <ul class="list-unstyled text-small">
+              <li>
+                <a class="text-muted" href="/">
+                  Local Networking
+                </a>
+              </li>{" "}
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Contact Us</h5>
+            <ul class="list-unstyled text-small">
+              <li>
+                <a class="text-muted" href="/">
+                  Team
+                </a>
+                <br />
+                <a class="text-muted" href="/">
+                  Project
+                </a>
+                <br />
+                <a class="text-muted" href="/">
+                  Terms of UsePrivacy Policy
+                </a>
+                <br />
+                <a class="text-muted" href="/">
+                  Privacy Policy
+                </a>
+              </li>{" "}
+            </ul>
+          </div>
         </div>
-        <div class="col-6 col-md">
-          <h5>Features</h5>
-          <ul class="list-unstyled text-small">
-            <li>
-              <a class="text-muted" href="/">
-                Reservation
-              </a>
-            </li>{" "}
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>{" "}
-          <ul class="list-unstyled text-small">
-            <li>
-              <a class="text-muted" href="/">
-                Local Networking
-              </a>
-            </li>{" "}
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Contact Us</h5>
-          <ul class="list-unstyled text-small">
-            <li>
-              <a class="text-muted" href="/">
-                Team
-              </a>
-              <br />
-              <a class="text-muted" href="/">
-                Project
-              </a>
-              <br />
-              <a class="text-muted" href="/">
-                Terms of UsePrivacy Policy
-              </a>
-              <br />
-              <a class="text-muted" href="/">
-                Privacy Policy
-              </a>
-            </li>{" "}
-          </ul>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
