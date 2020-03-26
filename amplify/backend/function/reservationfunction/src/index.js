57ef994a16255493a33dc813e88bbd7a7d1270e2
zhuochaozhong <zhuochaozhong2017@gmail.com>
2020-03-26 08:57:10 -0400

    initialize reservation functions

---
+++ amplify/backend/function/reservationfunction/src/index.js
@@ -0,0 +1,17 @@
+/* Amplify Params - DO NOT EDIT
+You can access the following resource attributes as environment variables from your Lambda function
+var environment = process.env.ENV
+var region = process.env.REGION
+var storageReservationtableName = process.env.STORAGE_RESERVATIONTABLE_NAME
+var storageReservationtableArn = process.env.STORAGE_RESERVATIONTABLE_ARN
+
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
